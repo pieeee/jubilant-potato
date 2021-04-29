12b6e3acd7972ecd355a418906c87b31d500bdef
pieeee <ahnafurp@gmail.com>
2021-04-29 15:48:25 +0600

    Menu page correction

--- src/components/menu-item/menuItem.component.js
+++ src/components/menu-item/menuItem.component.js
@@ -5,12 +5,19 @@ import { useStyles } from './menuItem.styles'
 
 const MenuItem = ({ title, path, imgURL, match, history }) => {
   const classes = useStyles()
+  const convertToRouteName = (title) =>
+    title
+      .replace(/\s/g, '_')
+      .replace(/[{()}]/g, '')
+      .toLowerCase()
   return (
     <Grid item xs={12} sm={6} md={4}>
       <div
         className={classes.menuItem}
         style={{ backgroundImage: `url(${imgURL})` }}
-        onClick={() => history.push(`${match.url}${path}`)}
+        onClick={() =>
+          history.push(`${match.url}shop/${convertToRouteName(title)}`)
+        }
       >
         <div className={classes.content}>
           <div className={classes.contentItem}>
